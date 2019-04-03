1cdb7746b38732220585ec0868ea6c38a9977bc2
Colin Sauze <cos@aber.ac.uk>
2019-04-03 14:48:21 +0100

    pep8/pylint fixes shell scripts for running example1/2 adding generating reports to example1/2

--- run_example1.py
+++ run_example1.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
 from makereport import generate_report
 import take_screenshot
-
+'''SnippySnap example application 1'''
 
 
 def run():
+    '''Run everything'''
 
     # obtain a firefox web driver for selenium
     browser = webdriver.Firefox()
 
-    #load the url
+    # load the url
     browser.get("http://localhost/example/example1.html")
 
-    browser.find_elements_by_css_selector("input[type='radio'][value='orange']")[0].click()
+    browser.find_elements_by_css_selector(
+            "input[type='radio'][value='orange']")[0].click()
 
     take_screenshot.take_screenshot(browser, 'example.png')
 
     browser.quit()
+
+    generate_report("outputs/report.html")
 
 
 if __name__ == '__main__':
--- run_example2.py
+++ run_example2.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
 from makereport import generate_report
 import take_screenshot
-
+'''SnippySnap demo application 2'''
 
 
 def run():
+    '''run's this example'''
 
     # obtain a firefox web driver for selenium
     browser = webdriver.Firefox()
 
-    #load the url
+    # load the url
     browser.get("http://localhost/example/example2.html")
 
-    browser.find_elements_by_css_selector("input[type='radio'][value='orange']")[0].click()
+    browser.find_elements_by_css_selector(
+            "input[type='radio'][value='orange']")[0].click()
 
     take_screenshot.take_screenshot(browser, 'example.png')
 
     browser.quit()
+
+    generate_report("outputs/report.html")
 
 
 if __name__ == '__main__':

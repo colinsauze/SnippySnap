23bc1a96b5a2c8ed2719501650a02cada4ee5b2d
cat <910384+catsmith@users.noreply.github.com>
2019-04-03 13:54:28 +0100

    selenium script fixes

--- run_example1.py
+++ run_example1.py
@@ -3,7 +3,7 @@ from makereport import generate_report
 import take_screenshot
 
 
-def wait_for(self, function_with_assertion, timeout=10):
+def wait_for(function_with_assertion, timeout=10):
     start_time = time.time()
     while time.time() - start_time < timeout:
         try:

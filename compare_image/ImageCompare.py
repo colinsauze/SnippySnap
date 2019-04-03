f26f9f9521db87bed6a9f12a7822badd7d865a83
Colin Sauze <cos@aber.ac.uk>
2019-04-03 10:31:31 +0100

    code cleanup

--- compare_image/ImageCompare.py
+++ compare_image/ImageCompare.py
@@ -1,52 +1,74 @@
 import cv2
 import numpy as np
 
-img1 = cv2.imread('1.png')
-print(img1.dtype,img1.shape)
-img2 = cv2.imread('2.png')
 
+def compare_file(file1, file2, changes_file, mask_file):
+    '''Compares and highlights the differences between two images
+    :param file1: The first image file
+    :param file2: The second image file
+    :param changes_file: File to highlight differences in
+    :param mask_file: The output showing a mask of only the changed pixels
+    :return: True if the image has changed, false if it hasn't
+    '''
 
-dt1 = np.dtype((np.uint32, (np.uint8, 4)))
+    img1 = cv2.imread(file1)
+    img2 = cv2.imread(file2)
 
-width = img1.shape[1]
-height = img1.shape[0]
+    dt1 = np.dtype((np.uint32, (np.uint8, 4)))
 
-img3 = np.ndarray(shape=[height, width], dtype=np.uint32)
+    width1 = img1.shape[1]
+    height1 = img1.shape[0]
+    width2 = img2.shape[1]
+    height2 = img2.shape[0]
 
-#output images
-img4 = np.ndarray(shape=[height, width,3 ], dtype=np.uint8)
-mask = np.ndarray(shape=[height, width], dtype=np.uint8)
+    if width1 != width2:
+        print("Error: width's different, image1 = ", width1, "image2 = ",
+              width2)
+        return
+    if height1 != height2:
+        print("Error: height's different, image1 = ", height1,
+              "image2 = ", height2)
+        return
 
-#print(img3.dtype,img3.shape)
+    # output images
+    changes_img = np.ndarray(shape=[height, width, 3], dtype=np.uint8)
+    mask_img = np.ndarray(shape=[height, width], dtype=np.uint8)
 
-#img3 = img1.view(dtype=dt1)
+    is_different = False
 
-for y in range(0,height):
-    for x in range(0,width):
-        #print(type(img1[y][x]))
-        b = img1[y][x][0]
-        g = img1[y][x][1]
-        r = img1[y][x][2]
-        v1 = r + (g*256) + (b * 256**2)
+    # Loops through every pixel in the image and compares it
+    # FIXME: this is really suboptimal but I couldn't numpy magic to work
+    for y in range(0, height):
+        for x in range(0, width):
+            # combine the R, G and B channels into one value for comparison
+            b = img1[y][x][0]
+            g = img1[y][x][1]
+            r = img1[y][x][2]
+            v1 = r + (g*256) + (b * 256**2)
 
-        b = img2[y][x][0]
-        g = img2[y][x][1]
-        r = img2[y][x][2]
-        v2 = r + (g*256) + (b * 256**2)
-        
-        if v1 != v2:
-            img4[y][x][0] = 0
-            img4[y][x][1] = 0
-            img4[y][x][2] = 255
-            mask[y][x]=255
-        else:
-            img4[y][x][0] = b
-            img4[y][x][1] = g
-            img4[y][x][2] = r
-            mask[y][x]=0
+            b = img2[y][x][0]
+            g = img2[y][x][1]
+            r = img2[y][x][2]
+            v2 = r + (g*256) + (b * 256**2)
 
+            # see if the pixels are different
+            # highlight the changes in both the changes image and mask
+            if v1 != v2:
+                # invert the changed pixels
+                changes_img[y][x][0] = 255 - img2[y][x][0]
+                changes_img[y][x][1] = 255 - img2[y][x][1]
+                changes_img[y][x][2] = 255 - img2[y][x][2]
+                # make mask white
+                mask_img[y][x] = 255
+                is_different = True
+            else:
+                # justcopy the pixels
+                changes_img[y][x][0] = b
+                changes_img[y][x][1] = g
+                changes_img[y][x][2] = r
+                # leave mask black
+                mask_img[y][x] = 0
 
-cv2.imwrite("test3.png",img4)
-cv2.imwrite("mask.png",mask)
-
-
+    cv2.imwrite(changes_img, changes_file)
+    cv2.imwrite(mask_img, mask_file)
+    return is_different

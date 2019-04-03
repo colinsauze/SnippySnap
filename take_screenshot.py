5c8800a45737779dcf18693c110a649fa73a45b8
Colin Sauze <cos@aber.ac.uk>
2019-04-03 13:54:45 +0100

    changing directory structure

--- take_screenshot.py
+++ take_screenshot.py
@@ -15,17 +15,33 @@ def take_screenshot(browser, filename):
     if os.path.exists("outputs") != True:
         os.mkdir("outputs")
 
+    if os.path.exists("outputs/screenshots") != True:
+        os.mkdir("outputs/screenshots")
 
-    browser.save_screenshot("outputs/"+filename)
+
+    browser.save_screenshot("outputs/screenshots/"+filename)
     is_different = False
     #compare with the previous screenshot (if it exists)
     if os.path.isfile("outputs/old_screenshots/"+filename) == True:
-        print("old image exists, comparing ","outputs/"+filename,"with outputs/old_screenshots/"+filename)
-        is_different = imagecompare.compare_image("outputs/"+filename, "outputs/old_screenshots/"+filename, "outputs/comparison-"+filename, "outputs/mask-"+filename)
+        print("old image exists, comparing ","outputs/screenshots/"+filename,"with outputs/old_screenshots/"+filename)
+        is_different = imagecompare.compare_image("outputs/screenshots/"+filename, "outputs/old_screenshots/"+filename, "outputs/comparison-"+filename, "outputs/mask-"+filename)
         if is_different == True:
             print(filename,"has changed")
+            status_file = open("outputs/"+filename+".status","w")
+            status_file.write("changed")
+            status_file.close()
         else:
             print(filename,"hasn't changed")
+            # delete the mask and comparison image if they exist
+            status_file = open("outputs/"+filename+".status","w")
+            status_file.write("unchanged")
+            status_file.close()
+
+        #copy the image for the report
+        print("copying outputs/old_screenshots/"+filename,"to outputs/report-"+filename)
+        shutil.copyfile("outputs/old_screenshots/"+filename,"outputs/report-"+filename)
+
+
     else:
         print("old file doesn't exist")
 
@@ -35,6 +51,6 @@ def take_screenshot(browser, filename):
         os.mkdir("outputs/old_screenshots")
 
     #copy the image
-    shutil.copyfile("outputs/"+filename,"outputs/old_screenshots/"+filename)
-    return same
+    shutil.copyfile("outputs/screenshots/"+filename,"outputs/old_screenshots/"+filename)
+    return is_different
 
